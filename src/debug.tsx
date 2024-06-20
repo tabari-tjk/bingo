f493077cbdb786371da96f9fb4c608faa7f1fa20
tabari <tabari@t-jk.co.jp>
2024-06-20 15:02:27 +0900

    デバッグ用複数クライアント

--- src/debug.tsx
+++ src/debug.tsx
@@ -1,14 +1,50 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import App from "./App";
 import Client from "./client.tsx";
+import { API_SERVER } from "./globals.tsx";
 
 export default function DebugClient({ clientNum }: { clientNum: number }) {
+    const [room_id, setRoomId] = useState<number>(0);
+    const [clients, setClients] = useState<React.JSX.Element[]>([]);
     return (<>
+        <input type="number" onChange={e => setRoomId(parseInt(e.target.value))} value={room_id} />
         <button onClick={() => {
-            window.location.reload();
-        }}>back</button>
-        {[...Array(clientNum).keys()].map(() => <>
-            <iframe src={window.location.href} ></iframe>
-        </>)}
+            fetch(API_SERVER + "api/login.php", {
+                method: "POST",
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ token: "" }),
+            })
+                .then(r => r.json())
+                .then(r => {
+                    if (r.token === null) {
+                        return;
+                    }
+                    setClients([...clients, <Client roomId={room_id} token={r.token} />]);
+                    setInterval(() => {
+                        fetch(API_SERVER + "api/heartbeat.php", {
+                            method: "POST",
+                            credentials: 'include',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ token: r.token }),
+                        })
+                            .then(r => r.json())
+                            .then(r => {
+                                if (!r[0]) {
+                                    alert("Error: userはタイムアウトしました");
+                                }
+                            });
+                    }, 10000);
+                });
+        }}>add client</button>
+        <div style={{ "display": "flex", "flexWrap": "wrap" }}>
+            {clients.map((c) => <div style={{ "border": "1px dotted" }}>
+                {c}
+            </div>)}
+        </div>
     </>);
 }
