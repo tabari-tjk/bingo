69c35ce096188ec71c265d8c7ecba725ddbd6627
tabari <tabari@t-jk.co.jp>
2024-07-01 14:03:12 +0900

    未使用ファイルの削除

--- php/api/api_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("database.php");
-$db = new DataBase();
-$room_id = $db->get_user_room_id($_GET["token"]);
-$is_gm = $db->is_user_gm($_GET["token"]);
-echo json_encode([$room_id, $is_gm]);
